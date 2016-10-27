16546919a91eba62c319e78d5d52be5d260c241b
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2016-10-26 19:01:49 -0600

    fix help doc typo, add more individual user metrics
    fix help doc typo, add more individual user metrics

--- globus-usage.R
+++ globus-usage.R
@@ -11,10 +11,17 @@ library("xlsx")
 
 option_list = list(
   make_option(c("-f", "--file"), type="character", default=NA, help="globus usage tranger file name", metavar="character"),
-  make_option(c("-o", "--out"), type="character", default="out.xslx", help="output file name [default= %default]", metavar="character"),
+  make_option(c("-o", "--out"), type="character", default="out.xlsx", help="output file name [default= %default]", metavar="character"),
   make_option(c("-s", "--start"), type="character", default=NA, help="the start date for globus transfers for the report (YYYY-MM-DD)", metavar="character"),
   make_option(c("-e", "--end"), type="character", default=NA, help="the end date for globus transfers for the report (YYYY-MM-DD)", metavar="character")
 ); 
+
+#option_list = list(
+#  make_option(c("-f", "--file"), type="character", default="Globus_Usage_Transfer_Detail.csv", help="globus usage tranger file name", metavar="character"),
+#  make_option(c("-o", "--out"), type="character", default="temp_out.xslx", help="output file name [default= %default]", metavar="character"),
+#  make_option(c("-s", "--start"), type="character", default="2015-10-01", help="the start date for globus transfers for the report (YYYY-MM-DD)", metavar="character"),
+#  make_option(c("-e", "--end"), type="character", default="2016-09-30", help="the end date for globus transfers for the report (YYYY-MM-DD)", metavar="character")
+#); 
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
@@ -84,7 +91,11 @@ transfermetrics$gt500mbpsls1gbps <- count(subset(orddataset, transfer_rate_mbps 
 transfermetrics$gt250mbpsls500mbps <- count(subset(orddataset, transfer_rate_mbps >= 250 & transfer_rate_mbps < 500))
 transfermetrics$gt50mbpsls250mbps <- count(subset(orddataset, transfer_rate_mbps >= 50 & transfer_rate_mbps < 250))
 transfermetrics$ls50mbps <- count(filter(orddataset, transfer_rate_mbps < 50))
-                          
+
+userdatametrics <-as.data.frame(orddataset %>% 
+                                  group_by(user_name) %>%                            # multiple group columns
+                                  summarise(total_bytes_transfered = sum(bytes_transferred),total_gbs_transfered= sum(bytes_transferred)/1000/1000/1000,files_successful = sum(successful), min_transfer_rate_mbps = min(transfer_rate_mbps), mean_transfer_rate_mbps = mean(transfer_rate_mbps), max_transfer_rate_mbps = max(transfer_rate_mbps)))  # multiple summary columns
+
 #write dataframe to csv file
 #write.csv(orddataset, file = outfile,row.names=FALSE)
 
@@ -93,5 +104,6 @@ write.xlsx(orddataset, file=outfile, sheetName="transfer-data")
 write.xlsx(uniqusers, file=outfile, sheetName="unique-users", append=TRUE)
 write.xlsx(uniqsources, file=outfile, sheetName="unique-sources", append=TRUE)
 write.xlsx(uniqdests, file=outfile, sheetName="unique-destinations", append=TRUE)
+write.xlsx(userdatametrics, file=outfile, sheetName="user-data-metrics", append=TRUE)
 write.xlsx(transfermetrics, file=outfile, sheetName="transfer-metrics", append=TRUE)
 
